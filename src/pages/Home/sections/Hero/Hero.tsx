d7b903380a5cc5530902e5350139721e106ce8e5
Rogério Duarte Xavier <rogdu2@gmail.com>
2025-03-15 18:53:18 -0300

    Mudanças do Background principal

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -1,19 +1,24 @@
-import { Button, Container, Grid, styled, Typography } from "@mui/material"
+import { Box, Container, Grid, styled, Typography } from "@mui/material"
 import Avatar from "../../../../assets/images/avatar.png"
 import DownloadIcon from '@mui/icons-material/Download';
 import EmailIcon from '@mui/icons-material/Email';
+import StyledButton from "../../../../components/StyledButton/StyledButton";
+import { AnimatedBackground } from "../../../../components/AnimatedBackground/AnimatedBackground";
 
 const Hero = () => {
 
-    const StyledHero = styled("div")(() => ({
-        backgroundColor: "black",
-        height: "100vh"
+    const StyledHero = styled("div")(({ theme }) => ({
+        backgroundColor: theme.palette.primary.main,
+        height: "100vh",
+        display:"flex",
+        alignItems:"center"
 
     }))
 
     const StyledImg = styled("img")(() => ({
-        width: "100%",
-        borderRadius: "50%"
+        width: "80%",
+        borderRadius: "50%",
+        border: '1px solid ${theme.palette.primary.main}'
 
     }))
 
@@ -22,24 +27,37 @@ const Hero = () => {
             <StyledHero>
                 <Container maxWidth="lg" >
                     <Grid container spacing={2}>
-                        <Grid item xs={12} md={4}>
-                            <StyledImg src={Avatar} />
+                        <Grid item xs={12} md={5}>
+                            <Box position="relative">
+                                <Box position="absolute" width={"150%"} top={"-100%"} right={"0"}>
+                            <AnimatedBackground />
+                                </Box>
+                                <Box position="absolute" textAlign={"center"}>
+                                <StyledImg src={Avatar} />
+                                </Box>
+                            </Box>
+                           
                         </Grid>
-                        <Grid item xs={12} md={8}>
-                            <Typography color="primary" variant="h1" textAlign="center">Rogério Duarte Xavier</Typography>
-                            <Typography color="primary" variant="h3" textAlign="center">Eu sou um futuro FrontEnd</Typography>
+                        <Grid item xs={12} md={7}>
+                            <Typography color="primary.contrastText" variant="h1" textAlign="center">Rogério Duarte Xavier</Typography>
+                            <Typography color="primary.contrastText" variant="h3" textAlign="center">Eu sou um futuro FrontEnd</Typography>
 
-                            <Grid container display="flex" justifyContent="center">
+                            <Grid container display="flex" justifyContent="center" spacing={2}>
                                 <Grid item xs={12} md={4} display="flex" justifyContent="center" >
-                                    <Button>
+
+                                    <StyledButton>
                                         <DownloadIcon />
-                                        Download CV</Button>
+                                        <Typography>Download CV</Typography>
+
+                                    </StyledButton>
                                 </Grid>
 
                                 <Grid item xs={12} md={4} display="flex" justifyContent="center">
-                                    <Button>
+                                    <StyledButton>
                                         <EmailIcon />
-                                        Contato</Button>
+                                        <Typography>Contato</Typography>
+
+                                    </StyledButton>
                                 </Grid>
                             </Grid>
                         </Grid>
